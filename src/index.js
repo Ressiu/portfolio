5a3b9ae1ab8e12516842c0a4443d100b5a5036e6
ressiu <joaocesarressio@gmail.com>
2024-02-17 13:07:17 -0300

    melhorias e backup
    adicionei o popup e fiz algumas melhorias.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,19 @@ function carregarpage() {
     let pagecontent = (document.getElementsByClassName(
         'site',
     )[0].style.display = 'block');
+
+    let popupalert = (document.getElementsByClassName(
+        'popupalert',
+    )[0].style.display = 'grid');
+
     let pageload = (document.getElementsByClassName(
         'loading',
     )[0].style.display = 'none');
+}
+
+function closepopup() {
+    let popup = (document.getElementsByClassName(
+        'popupalert',
+    )[0].style.display = 'none');
+    document.body.style.overflow = 'auto';
 }
